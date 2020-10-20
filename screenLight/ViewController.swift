c6a20b5ed0a2516ae8e279898b63fb3a79479bbc
Artme <artalebar@gmail.com>
2020-10-20 22:26:45 +0300

    error

--- screenLight/ViewController.swift
+++ screenLight/ViewController.swift
@@ -2,18 +2,27 @@
 //  ViewController.swift
 //  screenLight
 //
-//  Created by Артем Баранкевич on 20.10.2020.
+//  Created by Artem on 20.10.2020.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+   var isLightOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed() {
+        isLightOn.toggle()
+        if isLightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
+    
 }
 
