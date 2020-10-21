f869d2e7bfaec189e7ad88b7ffeae83268e451a5
Artme <artalebar@gmail.com>
2020-10-21 21:57:47 +0300

    Rainbow screen change

--- screenLight/ViewController.swift
+++ screenLight/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   var isLightOn = true
+   var screenColorNumber = 0
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -21,15 +21,35 @@ class ViewController: UIViewController {
     }
 
     fileprivate func changeUI() {
-        view.backgroundColor = isLightOn ? .white : .black
-    }
-    
-    @IBAction func buttonPressed() {
-        isLightOn.toggle()
-        changeUI()
+        switch screenColorNumber {
+        case 0:
+            view.backgroundColor = .white
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .orange
+        case 3:
+            view.backgroundColor = .yellow
+        case 4:
+            view.backgroundColor = .green
+        case 5:
+            view.backgroundColor = .systemTeal
+        case 6:
+            view.backgroundColor = .blue
+        case 7:
+            view.backgroundColor = .purple
+        default:
+            view.backgroundColor = .white
+            screenColorNumber = 0
         }
         
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        screenColorNumber += 1
+        changeUI()
+    }
+    
+}
     
 
 
